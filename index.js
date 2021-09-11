18489b97b17e3779024c624a8dff94be751b35f4
Ben S <benslinde@gmail.com>
2021-09-11 07:38:30 -0400

    added full mainmenu options and switch case for selection

--- index.js
+++ index.js
@@ -20,18 +20,50 @@ db.query('SELECT * FROM tableName', function (err, results) {
 });
 
 
-function manageEmployees () {
+function manageCompany () {
   inquirer
   .prompt([
     {
       type: 'list',
       message: 'What would you like to do?',
       name: 'mainMenu',
-      choices: ['View All Employees', 'Add Employee'],
+      choices: ['View All Departments', 'View all Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role' ],
     },
   ])
   .then((data) => {
+    console.log(data.mainMenu);
+    let userChoice = data.mainMenu;
+  
     //SWITCH CASE DEPEDNING ON MAINMENU VALUE
+
+switch (userChoice) {
+  case 'View All Departments':
+    console.log('Look at all those departments');
+    break;
+  case 'View all Roles':
+    console.log('So many roles from which to choose');
+    break;
+    case 'View All Employees':
+      console.log('Do we really need this many people');
+      break;
+    case 'Add a Department':
+      console.log('Grow the business, add a department');
+      break;
+      case 'Add a Role':
+    console.log('Add a Role');
+    break;
+    case 'Add an Employee':
+      console.log('Add en Employee');
+      break;
+    case 'Update an Employee Role':
+      console.log('Update an Employee Role');
+      break;
+  default:
+    console.log('something went wrong');
+    break;
+}
+
+
     //RUN A FUNCTION
 
     //Case of Add employee
@@ -40,7 +72,7 @@ function manageEmployees () {
     //CASE of UPDATE ROLE
       //RUN my update Role function
     
-    );
+  });
 }
 
 function ViewALLEMployess () {
@@ -54,17 +86,17 @@ function AddAnEmployee () {
 
   // add of this I will have an array of objects
 
-  .then((data)) => {
+  // .then((data)) => {
 
-    //INSERT
-    INSERT INTO employee (first_name, last_name)
-    VALUES
-      (varibaleinputfirstname, varibale input for last name);
+  //   //INSERT
+  //   INSERT INTO employee (first_name, last_name)
+  //   VALUES
+  //     (varibaleinputfirstname, varibale input for last name);
 
-    INSERT INTO role (title, salary, department_id)
-    VALUES
-      (variablestitle, salaryvariable, depavariable);
-  }
+  //   INSERT INTO role (title, salary, department_id)
+  //   VALUES
+  //     (variablestitle, salaryvariable, depavariable);
+  // });
 
 }
 
@@ -79,4 +111,4 @@ function updateEmployeeRole () {
 
 
 //Need to call the program to start it
-manageEmployees ();
+manageCompany ();
